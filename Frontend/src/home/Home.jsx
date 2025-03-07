4ac92820dd9dee5cceb62654695abca01e508c32
sanket jadhav <sanket.jadhav@hiddenbrains.in>
2025-03-07 15:48:50 +0530

    quck changes

--- Frontend/src/home/Home.jsx
+++ Frontend/src/home/Home.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import BusinessInfo from "./BusinessInfo";
-import PrimeMembers from "./primeMembers";
-import SuccessStories from "./successStories";
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import BusinessInfo from './BusinessInfo';
+import PrimeMembers from './primeMembers';
+import SuccessStories from './successStories';
 
 function Home() {
   return (
     <>
-      <div className="bg-blue-400">
+      <div className='bg-blue-400'>
         <Navbar />
         <BusinessInfo />
         {/* <PrimeMembers /> */}
